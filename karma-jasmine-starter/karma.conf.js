2f2c35ce56d803534fac8c6aa56dce723fd7daa0
Alexandra Marin <alexandra.marin01@gmail.com>
2017-07-30 22:51:09 +0300

    Update starter refs with babel+webpack

--- karma-jasmine-starter/karma.conf.js
+++ karma-jasmine-starter/karma.conf.js
@@ -1,11 +1,22 @@
 module.exports = function(config) {
-  config.set({
-    frameworks: ['jasmine'],
-    files: [
-      'spec/*.js'
-    ],
-     browsers: [
-     'Chrome'
-     ]
-  })
-}
+	config.set({
+		frameworks: ["jasmine"],
+		files: ["spec/*.js"],
+		browsers: ["Chrome"],
+		preprocessors: {
+			"spec/**/*.js": ["webpack"]
+		},
+		webpack: {
+			module: {
+				loaders: [
+					{
+						test: /\.js/,
+						exclude: /node_modules/,
+						loader: "babel-loader"
+					}
+				]
+			},
+			watch: true
+		}
+	});
+};
